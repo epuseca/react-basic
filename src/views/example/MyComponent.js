ab0489c036525c308c494c4609299199070c8b81
epu2seca <30srecord@gmail.com>
2025-04-08 23:35:54 +0700

    5.0

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -2,11 +2,33 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+
+    /* JSX html trong js
+    fragment
+    */
+    state = {
+        name: "LOC",
+        channel: "HEHE"
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>
-                Hello my component GIA LOC
-            </div>
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)}/>
+                    
+                    This is my channel: {this.state.channel}
+                </div>
+                <div className="second">
+                    Hello my component, My name is {this.state.name}
+                </div>
+            </>
         )
     }
 }
