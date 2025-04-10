ecbc110385af9f427df9b950ac3333e86de1c9b9
epu2seca <30srecord@gmail.com>
2025-04-10 14:26:16 +0700

    13. stateless/ state full

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -1,31 +1,9 @@
 import React from "react";
 
 class ChildComponent extends React.Component {
-
-
-    /* JSX html trong js
-    fragment
-    */
-    state = {
-        firstName: '',
-        lastName: ''
-    }
-    handleChangeFirstName = (event) => {
-        this.setState({
-            firstName: event.target.value,
-        })
-    }
-    handleChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-    handleSubmit = () => {
-        alert('Click me')
-    }
     // re-render
     render() {
-        console.log("Call render>> ", this.state)
+        // console.log("Call render>> ", this.state)
         console.log("Check props>> ", this.props)
         let { name, age, address, arrJobs } = this.props
         return (
@@ -46,5 +24,25 @@ class ChildComponent extends React.Component {
         )
     }
 }
+
+// const ChildComponent = (props) => {
+//     console.log('>>> check child props', props)
+//     let { arrJobs } = props
+//     return (
+//         <>
+//             <div className="job-lists">
+//                 {
+//                     arrJobs.map((item, index) => {
+//                         return (
+//                             <div key={item.id}>
+//                                 {item.title} - {item.salary}
+//                             </div>
+//                         )
+//                     })
+//                 }
+//             </div>
+//         </>
+//     )
+// }
 
 export default ChildComponent;
