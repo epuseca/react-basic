db70d8fa7b35e560c69f75cdcc7354ea3315a9fe
epu2seca <30srecord@gmail.com>
2025-04-10 09:05:00 +0700

    7 dom event, arrow function

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -16,17 +16,24 @@ class MyComponent extends React.Component {
             name: event.target.value
         })
     }
+    // Arrow function
+    handleClickButton = () => {
+        alert('Click meee')
+    }
 
     render() {
         return (
             <>
                 <div className="first">
-                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)}/>
-                    
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)} />
+
                     This is my channel: {this.state.channel}
                 </div>
                 <div className="second">
                     Hello my component, My name is {this.state.name}
+                </div>
+                <div className="third">
+                    <button onClick= { () => this.handleClickButton() }>Click me</button>
                 </div>
             </>
         )
