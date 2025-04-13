9fe18653da8d4f6cc846bb72168d4ea92b27c21f
epu2seca <30srecord@gmail.com>
2025-04-13 16:55:35 +0700

    26.1 push lai thêm list item

--- src/views/Users/ListUser.js
+++ src/views/Users/ListUser.js
@@ -1,21 +1,37 @@
 import React from "react";
 import axios from "axios";
-
+import './ListUser.scss'
 class ListUser extends React.Component {
-    async componentDidMount() {
-        // axios.get('https://reqres.in/api/users?page=2')
-        //     .then(res => {
-        //         console.log('Check res:', res.data.data)
-        //     })
-        
-        let res = await axios.get('https://reqres.in/api/users?page=2')
-        console.log('Check res:', res.data.data)
-
+    state = {
+        listUsers: []
     }
-
+    async componentDidMount() {
+        let res = await axios.get('https://reqres.in/api/users?page=2')
+        this.setState({
+            listUsers: res && res.data && res.data.data ? res.data.data : []
+        })
+        console.log('Check res:', res.data.data)
+    }
     render() {
+        let { listUsers } = this.state
         return (
-            <div>Helle List Users</div>
+            <div className="list-user-container">
+                <div className="title">
+                    Fetch all list users
+                </div>
+                <div className="list-user-content">
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            return (
+                                <div className="child" key={item.id}>
+                                    {index + 1} - {item.first_name} {item.last_name}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+            </div>
         )
     }
 }
